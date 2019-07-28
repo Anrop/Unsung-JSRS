778e8b49568cdb03c9efcf083ef580d1c246bda5
Björn Dahlgren <bjorn@dahlgren.at>
2019-07-28 16:34:18 +0200

    M60 on vehicles

--- addons/m60/config.cpp
+++ addons/m60/config.cpp
@@ -4,7 +4,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = 1.0;
-        requiredAddons[] = {"uns_weap_c", "JSRS_SOUNDMOD_Weapons"};
+        requiredAddons[] = {"uns_armour_c", "uns_weap_c", "JSRS_SOUNDMOD_Weapons"};
         addonRootClass = "uns_weap_c";
         author = "Anrop";
         url = "https://www.anrop.se";
@@ -58,6 +58,28 @@ class CfgWeapons
             class SilencedSound: SilencedSound
             {
                 soundSetShot[] = {"jsrs_rifle_shake_soundset", "jsrs_m60e4_shot_silenced_soundset", "jsrs_spmg_shell_soundset", "jsrs_7x62mm_sd_reflector_1"};
+            };
+        };
+    };
+
+    class uns_MGun_base;
+
+    class Uns_LMG_v: uns_MGun_base {
+        class FullAuto: uns_MGun_base {
+            class StandardSound;
+        };
+    };
+
+    class uns_M60_v : Uns_LMG_v 
+    {
+        drysound[] = {"\jsrs_soundmod\jsrs_soundmod_snd_weapons\sounds\dry\navid_dry.ogg", 1.5, 1, 15};
+        reloadmagazinesound[] = {"\jsrs_soundmod\jsrs_soundmod_snd_weapons\sounds\reload\navid_reload.ogg", 2, 1, 35};
+
+        class FullAuto: FullAuto
+        {
+            class StandardSound: StandardSound
+            {
+                soundSetShot[] = {"jsrs_m60e4_shot_soundset", "jsrs_spmg_shell_soundset", "jsrs_7x62mm_reflector_1"};
             };
         };
     };
