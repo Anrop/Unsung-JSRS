455004b9fd778bb263dfcd460260f4c08b250648
Björn Dahlgren <bjorn@dahlgren.at>
2019-07-28 14:13:31 +0200

    M2

---
+++ addons/m2/config.cpp
@@ -0,0 +1,39 @@
+class CfgPatches {
+    class anrop_unsung_jsrs_weapons_m2 {
+        name = "anrop_unsung_jsrs_weapons_m2";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"uns_weap_c", "JSRS_SOUNDMOD_Weapons"};
+        addonRootClass = "uns_weap_c";
+        author = "Anrop";
+        url = "https://www.anrop.se";
+    };
+};
+
+class CfgWeapons 
+{
+    class MGun;
+
+    class uns_MGun_base: MGun {};
+
+    class Uns_HMG_v: uns_MGun_base
+    {
+        class FullAuto: uns_MGun_base
+        {
+            class StandardSound;
+            class SilencedSound;
+        };
+    };
+    
+    class uns_M2 : Uns_HMG_v 
+    {
+        class FullAuto: FullAuto
+        {
+            class StandardSound: StandardSound
+            {
+                soundSetShot[] = {"jsrs_m2_shot_soundset", "jsrs_gunner_casing", "jsrs_hmg_reflector_1"};
+            };
+        };
+    };
+};
